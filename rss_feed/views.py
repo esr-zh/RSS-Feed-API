dd49482765ebf276db8cbbd2bf03f648d9915396
esr-zh <israh.zahid@ozu.edu.tr>
2023-12-19 01:35:24 +0300

    fixed a major database issue causing 500 server error

--- rss_feed/views.py
+++ rss_feed/views.py
@@ -15,27 +15,36 @@ def parse_rss():
     existing_feed = RSSFeed.query.filter_by(url=url).first()
 
     if existing_feed and datetime.now() - existing_feed.last_parsed < timedelta(minutes=10):
+        # Return existing articles if the feed was parsed less than 10 minutes ago
         articles = [{'title': article.title, 'description': article.description} for article in existing_feed.articles]
     else:
         articles_data = parse_rss_feed(url)
 
-        if existing_feed is None:
+        if existing_feed:
+            # Update last parsed time
+            existing_feed.last_parsed = datetime.utcnow()
+        else:
+            # Create new RSS feed entry
             existing_feed = RSSFeed(url=url, last_parsed=datetime.utcnow())
             db.session.add(existing_feed)
             db.session.flush()
 
-        existing_feed.last_parsed = datetime.utcnow()
-        existing_feed.articles = []
-
+        # Update or add new articles
         for article_data in articles_data:
-            article = Article(title=article_data['title'], description=article_data['description'],
-                              rss_feed_id=existing_feed.id)
-            existing_feed.articles.append(article)
+            article = Article.query.filter_by(title=article_data['title'], rss_feed_id=existing_feed.id).first()
+            if article:
+                # Update existing article
+                article.description = article_data['description']
+            else:
+                # Add new article
+                new_article = Article(title=article_data['title'], description=article_data['description'], rss_feed_id=existing_feed.id)
+                db.session.add(new_article)
 
         db.session.commit()
         articles = articles_data
 
     return jsonify(articles)
+
 
 
 @views.route('/show-data', methods=['GET'])
